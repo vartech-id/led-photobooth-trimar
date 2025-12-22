95eafa93dc33cab469ace4f94a918248a581c38c
PvtMilo <flip360.id.solo@gmail.com>
2025-12-22 13:22:51 +0700

    final

--- backend/scripts/toWeb.bat
+++ backend/scripts/toWeb.bat
@@ -1,6 +1,6 @@
 @echo off
-"C:\Windows\nircmd.exe" win activate title "pb-cimb-niaga"
+"C:\Windows\nircmd.exe" win activate title "allianz"
 ping -n 1 127.0.0.1 >nul
-"C:\Windows\nircmd.exe" win activate title "pb-cimb-niaga"
+"C:\Windows\nircmd.exe" win activate title "allianz"
 ping -n 1 127.0.0.1 >nul
-"C:\Windows\nircmd.exe" win activate title "pb-cimb-niaga"
+"C:\Windows\nircmd.exe" win activate title "allianz"
